94369db6c83072dda2ff4a8d14174a8e8fa905d6
Pravallika Gajul <pravsgajul@gmail.com>
2023-01-30 13:25:20 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,2 +1,22 @@
 import cv2
-import numpy
+import mediapipe as mp
+
+mpDraw = mp.solutions.drawing_utils
+mpPose= mp.solutions.pose
+pose = mpPose.Pose()
+
+cap = cv2.VideoCapture("KneeBendVideo.mp4")
+
+while True:
+    #success variable tells us whether the image is being captured successfully.
+    success,img=cap.read()
+    imgRGB=cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
+    result = pose.process(imgRGB)
+    print(result.pose_landmarks)
+    print("------------------------------------")
+    if result.pose_landmarks:
+        mpDraw.draw_landmarks(img,result.pose_landmarks,mpPose.POSE_CONNECTIONS)
+
+
+cv2.imshow("img",imgRGB)
+cv2.waitKey(5)
