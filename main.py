dfcfb3d7a61d46b6ac88eccff1d716027b5ffadf
Pravallika Gajul <pravsgajul@gmail.com>
2023-01-30 16:54:51 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,22 +1,42 @@
 import cv2
+import math
 import mediapipe as mp
+
+def getAngle(a, b, c):
+    ang = math.degrees(math.atan2(c[1]-b[1], c[0]-b[0]) - math.atan2(a[1]-b[1], a[0]-b[0]))
+    return ang + 360 if ang < 0 else ang
 
 mpDraw = mp.solutions.drawing_utils
 mpPose= mp.solutions.pose
 pose = mpPose.Pose()
 
 cap = cv2.VideoCapture("KneeBendVideo.mp4")
+bent = False
+counter=0
 
 while True:
     #success variable tells us whether the image is being captured successfully.
     success,img=cap.read()
     imgRGB=cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
     result = pose.process(imgRGB)
-    print(result.pose_landmarks)
-    print("------------------------------------")
+    #print(result.pose_landmarks)
+    #print("------------------------------------")
     if result.pose_landmarks:
         mpDraw.draw_landmarks(img,result.pose_landmarks,mpPose.POSE_CONNECTIONS)
+        points={}
+        for id,lm in enumerate(result.pose_landmarks.landmark):
+            #height, width, chanel
+            h,w,d=img.shape
+            #converting coordinates to pixels
+            cx,cy=int(lm.x*w),int(lm.y*h)
+            #print(id,lm,cx,cy)
+            points[id]=(cx,cy)
 
+        cv2.circle(img,points[23],15,(255,0,0))
+        cv2.circle(img,points[25],15,(255,0,0))
+        cv2.circle(img,points[27],15,(255,0,0))
+        print("---------------------")
+        print(getAngle(points[23],points[25],points[27]))
 
-cv2.imshow("img",imgRGB)
-cv2.waitKey(5)
+    cv2.imshow("img",img)
+    cv2.waitKey(5)
