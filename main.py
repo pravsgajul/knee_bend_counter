6ec6ee09ee9ffc1c1542a55ac900a501e6741bb0
Pravallika Gajul <pravsgajul@gmail.com>
2023-01-31 12:27:55 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import cv2
 import math
 import mediapipe as mp
 import timeit
+from datetime import datetime
+import time
 
 def getAngle(a, b, c):
     ang = math.degrees(math.atan2(c[1]-b[1], c[0]-b[0]) - math.atan2(a[1]-b[1], a[0]-b[0]))
@@ -12,9 +14,16 @@ mpPose= mp.solutions.pose
 pose = mpPose.Pose()
 
 cap = cv2.VideoCapture("KneeBendVideo.mp4")
-bent = False
-counter=0
+
 start=0
+now=datetime.now()
+start_time=now.strftime("%H:%M:%S")
+st_time =0
+en_time = 0
+ho_time=0
+bended = 0
+end_time="0:0:0"
+duration=0
 
 while True:
     #success variable tells us whether the image is being captured successfully.
@@ -24,7 +33,7 @@ while True:
     #print(result.pose_landmarks)
     #print("------------------------------------")
     if result.pose_landmarks:
-        mpDraw.draw_landmarks(img,result.pose_landmarks,mpPose.POSE_CONNECTIONS)
+        #mpDraw.draw_landmarks(img,result.pose_landmarks,mpPose.POSE_CONNECTIONS)
         points={}
         for id,lm in enumerate(result.pose_landmarks.landmark):
             #height, width, chanel
@@ -40,19 +49,28 @@ while True:
         
         angle = getAngle(points[23],points[25],points[27])
         
-        start = timeit.default_timer()
-        if not bent and angle<140:
-            print("Bent")
-            bent=True
-            counter+=1
-        if angle>140:
-            bent=False
+        if angle<140: 
+            if st_time==0 :
+                st_time = time.time_ns()
+            ho_time = (time.time_ns()-st_time)/1000000000
+        if angle>140 and st_time != 0:
+            en_time = time.time_ns()
+            diff = (en_time - st_time)/1000000000
+            if(diff >= 8.0 ) :
+                bended+= 1
+            st_time = 0
+            en_time = 0
+            ho_time = 0
 
     cv2.line(img, points[23], points[25], (0,0,0), 5)
     cv2.line(img, points[25], points[27], (0,0,0), 5)
-    cv2.putText(img,str(counter),(100,150),cv2.FONT_HERSHEY_SCRIPT_SIMPLEX,2,(55,55,55),8)
-    cv2.putText(img,str(start),(300,150),cv2.FONT_HERSHEY_SCRIPT_SIMPLEX,2,(55,55,55),8)
-    cv2.putText(img,str(reptimer),(400,150),cv2.FONT_HERSHEY_SCRIPT_SIMPLEX,2,(55,55,55),8)
+
+    cv2.putText(img,"Rep Count",(100,50),cv2.FONT_HERSHEY_PLAIN,1,(0,0,0),1)
+    cv2.putText(img,str(bended),(100,100),cv2.FONT_HERSHEY_SCRIPT_SIMPLEX,1,(0,0,0),4)
+    cv2.putText(img,"Hold Time",(250,50),cv2.FONT_HERSHEY_PLAIN,1,(0,0,0),1)
+    cv2.putText(img,"%.2f"%ho_time,(250,100),cv2.FONT_HERSHEY_SCRIPT_SIMPLEX,1,(0,0,0),4)
+    cv2.putText(img,"Angle",(500,50),cv2.FONT_HERSHEY_PLAIN,1,(0,0,0),1)
+    cv2.putText(img,"%.2f"%angle,(500,100),cv2.FONT_HERSHEY_SCRIPT_SIMPLEX,1,(0,0,0),4)
 
     cv2.imshow("img",img)
     cv2.waitKey(5)
